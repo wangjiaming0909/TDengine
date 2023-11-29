fef08426bd4ec22539d27be2864df5a6bb4e575c
wangjiaming0909 <604227650@qq.com>
2023-11-29 19:07:23 +0800

    support tsma catalog cache

--- source/libs/nodes/src/nodesCodeFuncs.c
+++ source/libs/nodes/src/nodesCodeFuncs.c
@@ -7071,6 +7071,7 @@ static int32_t createTSMAStmtToJson(const void* pObj, SJson* pJson) {
   if (TSDB_CODE_SUCCESS == code) {
     code = tjsonAddObject(pJson, jkCreateTSMAStmtpOptions, nodeToJson, pNode->pOptions);
   }
+  return code;
 }
 
 static int32_t jsonToCreateTSMAStmt(const SJson* pJson, void* pObj) {
@@ -7086,7 +7087,7 @@ static int32_t jsonToCreateTSMAStmt(const SJson* pJson, void* pObj) {
     code = tjsonGetStringValue(pJson, jkCreateTSMAStmtTableName, pNode->tableName);
   }
   if (TSDB_CODE_SUCCESS == code) {
-    code = jsonToNodeObject(pJson, jkCreateTSMAStmtpOptions, &pNode->pOptions);
+    code = jsonToNodeObject(pJson, jkCreateTSMAStmtpOptions, (SNode**)&pNode->pOptions);
   }
   return code;
 }
