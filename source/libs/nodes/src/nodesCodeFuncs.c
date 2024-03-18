33e0b4f28c21e59d5232909b7cf338fb97edc328
wangjiaming0909 <604227650@qq.com>
2024-03-18 17:55:24 +0800

    find TODO

--- source/libs/nodes/src/nodesCodeFuncs.c
+++ source/libs/nodes/src/nodesCodeFuncs.c
@@ -4237,10 +4237,6 @@ static int32_t realTableNodeToJson(const void* pObj, SJson* pJson) {
   if (TSDB_CODE_SUCCESS == code) {
     code = tjsonAddTArray(pJson, jkRealTableSmaIndexes, tableIndexInfoToJson, pNode->pSmaIndexes);
   }
-  if (TSDB_CODE_SUCCESS == code) {
-    //TODO
-    //code = tjsonAddTArray(SJson *pJson, const char *pName, FToJson func, const SArray *pArray);
-  }
 
   return code;
 }
@@ -4265,9 +4261,6 @@ static int32_t jsonToRealTableNode(const SJson* pJson, void* pObj) {
   if (TSDB_CODE_SUCCESS == code) {
     code =
         tjsonToTArray(pJson, jkRealTableSmaIndexes, jsonToTableIndexInfo, &pNode->pSmaIndexes, sizeof(STableIndexInfo));
-  }
-  if (TSDB_CODE_SUCCESS == code) {
-    //TODO
   }
 
   return code;
